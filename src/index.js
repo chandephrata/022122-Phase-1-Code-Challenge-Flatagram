e61f392482c389bb49b47a0d1c8c190d550316a4
Chandelier Ephrata <chandephrata@gmail.com>
2022-03-04 11:54:04 -0700

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // write your code here
+const flatagramtApi = "http://localhost:3000/images";
+const flat=document.getElementById('flatagram')
+    
+        fetch(flatagramtApi)
+        .then((response)=> response.json())
+        .then(seeImage)
+
+function seeImage(flatArr){
+    flatArr.forEach(addFlatagramImageToPage);
+}
+function addFlatagramImageToPage(flata){
+    const flatImage = document.getElementById('img');
+    flatImage.src = flata.image;
+    flatImage.addEventListener('click',()=>{ seeImage(flata)
+    })
+}
